3ea5cc4a41279039841b97a9ce191bb52c3c0257
Bryan Melo <bmelo650@gmail.com>
2024-01-05 12:35:04 -0800

    Added document handling and interface packages

---
+++ text_manipulator/interface/user_interface.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+
+def create_user_interface():
+    # Create the main window
+    window = tk.Tk()
+
+    # Set the window title
+    window.title("Text Manipulation Tool")
+
+    # Set the window size
+    window.geometry("700x500")  # width x height
+
+    return window
+
+
